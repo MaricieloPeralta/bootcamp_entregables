b585721023bd842f0282e748c88ba3d0931ba7db
kmy1 <98854090+kmly2@users.noreply.github.com>
2026-03-18 14:12:15 -0500

    Create verifyToken.js

---
+++ nodejs/dia5/middlewares/verifyToken.js
@@ -0,0 +1,21 @@
+const jwt = require('jsonwebtoken')
+
+const verifyToken = (req, res, next) => {
+    try {
+
+        const authHeader = req.headers["authorization"];
+        if (!authHeader) {
+            return res.status(401).json({ error: "No token enviado" });            
+        }
+        
+        const token = authHeader.split(" ")[1];
+        const decoded = jwt.verify(token, process.env.JWT_SECRET || "clave_desarrollo");
+        req.usuario = decoded;
+        next();
+
+    } catch (error){
+        res.status(401).json({ error: "Token invalido o expirado" });
+    }
+}
+
+module.exports = verifyToken;
